ca8e4839aac93a8f1a59c86a4b12228b13ba0bfd
John-Michael Riley <johnmichael.w.riley@gmail.com>
2021-03-18 13:25:51 -0400

    add associations examples

---
+++ associations.js
@@ -0,0 +1,27 @@
+const {User, Post, Subbreaddit} = require('./models')
+
+const getUserWithPosts = async (id) => {
+  // lazy-loading
+  // const user = await User.findByPk(id)
+  // const userPosts = await user.getPosts()
+  // console.log('user', user.toJSON())
+  // console.log('user posts', userPosts.map(post => post.toJSON()))
+
+  // eager loading
+  // fetching user and associated posts
+  const userWithPosts = await User.findByPk(id, {
+    include: Post
+  })
+  console.log(userWithPosts.toJSON())
+
+  // fetch user, their posts, and each posts subbreaddit
+  const userPostsAndSubs = await User.findByPk(id, {
+    include: {model: Post, include: Subbreaddit}
+  })
+
+  console.log('userObj', userPostsAndSubs.toJSON())
+  const userPosts = userPostsAndSubs.Posts
+  console.log('posts subbreaddit',userPosts[0].Subbreaddit.toJSON())
+}
+
+getUserWithPosts(1)
